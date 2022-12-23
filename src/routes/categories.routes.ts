04b5e0aaf483b978a5101633a28b38ff348c9eaa
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2022-12-23 16:47:06 -0300

    Aula 03: Iniciando SOLID com o principio de Single Responsability

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,20 @@
 import Router from "express";
 import { Category } from "../models/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 export const categoriesRoutes = Router();
 
 const categoriesRepository = new CategoriesRepository();
-categoriesRoutes.get("/", (request, response) =>{
-   
+categoriesRoutes.get("/", (request, response) => {
+
     const categories = categoriesRepository.list();
     response.status(200).json(categories);
 });
 
-categoriesRoutes.post("/", (request, response) =>{
-    const {name, description} = request.body;
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-    
-    if(categoryAlreadyExists){
-        return response.status(400).json({message: "Category already exists"});
-    }
-    
-    categoriesRepository.create({name, description});
-    return response.status(201).json({message: "Created!"})
-    
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    createCategoryService.execute({name, description});
+    return response.status(201).json({ message: "Created!" })
+
 });
