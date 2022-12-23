cdda034094ace046859ef05b253931d5d4481e29
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2022-12-23 11:29:18 -0300

    aula02: separando as rotas da aplicação

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,13 @@
 import Router from "express";
-
+import {v4 as uuidv4} from "uuid";
 export const categoriesRoutes = Router();
 const categories = []
-categoriesRoutes.get("/categories", (request, response) =>{
+categoriesRoutes.get("/", (request, response) =>{
     response.status(200).json(categories);
 });
 
-categoriesRoutes.post("/categories", (request, response) =>{
+categoriesRoutes.post("/", (request, response) =>{
     const {name, description} = request.body;
+    categories.push({name, description});
     return response.status(201).json({message: "Created"});
 });
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
 import express from "express";
 import cors from "cors";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 const port = 3333;
 app.use(cors());
 app.use(express.json());
-const curses = [
-    {name: "Java"},
-    {name: "Javascrip"},
-    {name: "PHP"},
-]
+
 app.get("/home", (request, response) =>{
     response.status(200).json({message: "HELLO WORLD!"});
 });
+
+app.use("/categories", categoriesRoutes);
 
 
 
