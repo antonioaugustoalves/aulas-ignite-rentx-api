8a5168983636291d2ddf7df79dcc2ab8dd2b7870
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2022-12-24 11:59:21 -0300

    Aula05: Fazendo o upload e leitura dos arquivos csv

--- src/modules/car/use-cases/import-categories/ImportCategoriesUseCase.ts
+++ src/modules/car/use-cases/import-categories/ImportCategoriesUseCase.ts
@@ -1,5 +1,12 @@
+import fs from "fs";
+import {parse} from "csv-parse";
 export class ImportCategoriesUseCase {
     execute(file: Express.Multer.File): void{
-        console.log(file);
+        const stream = fs.createReadStream(file.path);
+        const parseFile = parse();
+        stream.pipe(parseFile);
+        parseFile.on("data", async line =>{
+            console.log(line);
+        });
     }
 }
