3481060fddc01e9ebfe1eca7ba8a880214aea068
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2022-12-24 16:25:35 -0300

    Aula 06: Finalizando a documentaçao de listagem de categorias e adicionando o comando que exclui auomaticamente os arquivos temporarios da pasta temp

--- src/modules/car/use-cases/import-categories/ImportCategoriesUseCase.ts
+++ src/modules/car/use-cases/import-categories/ImportCategoriesUseCase.ts
@@ -25,19 +25,20 @@ export class ImportCategoriesUseCase {
                     description
                 });
             }).on("end", () => {
+                fs.promises.unlink(file.path);
                 resolve(categories);
             }).on("error", (err) => {
                 reject(err);
             });
         });
     }
-    async execute(file: Express.Multer.File): Promise<void>  {
+    async execute(file: Express.Multer.File): Promise<void> {
         const categories = await this.loadCategories(file);
-        categories.map(async (category) =>{
-            const {name, description} = category;
+        categories.map(async (category) => {
+            const { name, description } = category;
             const existCategory = this.categoriesRepository.findByName(name);
 
-            if(!existCategory) {
+            if (!existCategory) {
                 this.categoriesRepository.create({
                     name, description
                 });
