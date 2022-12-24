d3f453204b3375acbcb081a5b2c61179d9b83011
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2022-12-24 16:10:24 -0300

    Aula 06: Iniciando a documentação com o swagger-express-ui

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+import swagggerUI from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 import { router } from "./routes";
 
 const app = express();
 const port = 3333;
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/docs", swagggerUI.serve, swagggerUI.setup(swaggerFile));
 app.get("/home", (request, response) =>{
     response.status(200).json({message: "HELLO WORLD!"});
 });
