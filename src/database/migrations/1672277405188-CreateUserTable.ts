e82a9ac5b7868bc03f8a36e8b55daf91979a7334
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2022-12-28 22:30:57 -0300

    Aula 09: Iniciando o cadastro de usuaários do aplicativo rentx

---
+++ src/database/migrations/1672277405188-CreateUserTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUserTable1672277405188 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
