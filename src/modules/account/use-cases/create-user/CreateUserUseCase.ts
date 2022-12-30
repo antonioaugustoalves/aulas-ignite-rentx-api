eb7c57858b42a2e573d683ba8f80c4770d8feb4e
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2022-12-30 18:47:14 -0300

    Aula 09: Adicionando criptografia de senha com a biblioteca BCrypt

--- src/modules/account/use-cases/create-user/CreateUserUseCase.ts
+++ src/modules/account/use-cases/create-user/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe"; 
+import {hash} from "bcrypt";
 import { UsersRepository } from "../../repositories/implementation/UsersRepository";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -10,8 +11,9 @@ class CreateUserUseCase {
         private usersRepository: UsersRepository
     ){}
     async execute({name, email, password, driverLicense}:ICreateUserDTO): Promise<void>{
+        const passwordHash = await hash(password, 8)
         await this.usersRepository.create({
-            name, email, password, driverLicense
+            name, email, password: passwordHash, driverLicense
         })
     }
 }
