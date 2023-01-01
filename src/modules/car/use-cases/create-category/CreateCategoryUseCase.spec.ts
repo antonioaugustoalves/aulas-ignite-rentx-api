1c174d66211a404c8f20f9cbcff1dc1fc5619dfc
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-01-01 15:16:30 -0300

    Aula 11: Testes de software com jest para mostrar quando os testes devem falhar

--- src/modules/car/use-cases/create-category/CreateCategoryUseCase.spec.ts
+++ src/modules/car/use-cases/create-category/CreateCategoryUseCase.spec.ts
@@ -4,5 +4,11 @@ describe("Create category", () =>{
         const result = 4;
 
         expect(soma).toBe(result);
+    });
+
+    it("Espero que 2 + 2 seja diferente de 5",() =>{
+        const soma = 2+2;
+        const result = 5;
+        expect(soma).not.toBe(result);
     })
 })
