91f4da17bacfeeb699629365921bab8db5e39e21
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-01-02 20:46:58 -0300

    Aula 11: Criação de um repositorio in memory para testa com o jest a criação de uma nova categoria

---
+++ src/modules/car/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,23 @@
+import { Category } from "../../entities/Category";
+import { ICategoriesRepository, ICategoryDTO } from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+    categories:Category[] = [];
+
+    async create({ name, description }: ICategoryDTO): Promise<void> {
+        const category = new Category();
+        Object.assign(category, { name, description });
+        this.categories.push(category);
+    }
+    async findByName(name: string): Promise<Category> {
+        const category = this.categories.find(category => category.name === name);
+        return category;
+    }
+    async list(): Promise<Category[]> {
+        const listAll = this.categories;
+        return listAll;
+    }
+    
+}
+
+export {CategoriesRepositoryInMemory}
