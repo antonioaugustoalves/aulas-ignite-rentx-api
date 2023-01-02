67026321533d46907c582392f40fa4b5a23c8b54
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-01-02 20:53:57 -0300

    Aula 11: Testando o erro de criação de categorias com nomes iguais. Aqui esperamos um erro do tipo AppError

--- src/modules/car/use-cases/create-category/CreateCategoryUseCase.spec.ts
+++ src/modules/car/use-cases/create-category/CreateCategoryUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../errors/AppError";
 import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
@@ -23,6 +24,26 @@ describe("Create category", () => {
         const categoryCreated = await categoriesRepository.findByName(category.name);
         console.log(categoryCreated);
         expect(categoryCreated).toHaveProperty("id")
+    });
+
+    it("Should not  be able to create a new category if it already exists", async () => {
+        expect(async () =>{
+            const category = {
+                name: "Category Test",
+                description: "Test description"
+            }
+            await createCategory.execute({
+                name: category.name,
+                description: category.description
+            });
+    
+            await createCategory.execute({
+                name: category.name,
+                description: category.description
+            });
+        }).rejects.toBeInstanceOf(AppError)
+
+        
     });
 
 
