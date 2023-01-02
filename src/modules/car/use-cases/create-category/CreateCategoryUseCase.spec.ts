b10ae74b8a0eef9ad26a12ff7367b9d03282d339
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-01-02 20:46:39 -0300

    Aula 11: Criação de um repositorio in memory para testa com o jest a criação de uma nova categoria

--- src/modules/car/use-cases/create-category/CreateCategoryUseCase.spec.ts
+++ src/modules/car/use-cases/create-category/CreateCategoryUseCase.spec.ts
@@ -1,14 +1,29 @@
-describe("Create category", () =>{
-    it("Espero que 2 + 2 = 4", () =>{
-        const soma = 2+2;
-        const result = 4;
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-        expect(soma).toBe(result);
+describe("Create category", () => {
+    let categoriesRepository: CategoriesRepositoryInMemory;
+    let createCategory: CreateCategoryUseCase;
+
+    beforeEach(() => {
+        categoriesRepository = new CategoriesRepositoryInMemory();
+        createCategory = new CreateCategoryUseCase(categoriesRepository);
     });
 
-    it("Espero que 2 + 2 seja diferente de 5",() =>{
-        const soma = 2+2;
-        const result = 5;
-        expect(soma).not.toBe(result);
-    })
+    it("Should be able to create a new category", async () => {
+        const category = {
+            name: "Category Test",
+            description: "Test description"
+        }
+        await createCategory.execute({
+            name: category.name,
+            description: category.description
+        });
+
+        const categoryCreated = await categoriesRepository.findByName(category.name);
+        console.log(categoryCreated);
+        expect(categoryCreated).toHaveProperty("id")
+    });
+
+
 })
