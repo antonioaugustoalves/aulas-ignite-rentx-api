6f89a120fd7d0ce802c5670fdf8fdbf9324a3fd8
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-01-04 22:04:30 -0300

    Aula 12: Rodando a migration de carros e criando a primeira chave estrangeira

--- src/shared/infra/typeorm/migrations/1672879657781-CreateCarTable.ts
+++ src/shared/infra/typeorm/migrations/1672879657781-CreateCarTable.ts
@@ -1,11 +1,68 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateCarTable1672879657781 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table(
+                {
+                    name: "cars",
+                    columns: [
+                        {name:"id", type:"uuid", isPrimary: true},
+                        {name:"name", type:"varchar"},
+                        {name:"description", type:"varchar"},
+                        {name:"dailyRate", type:"numeric"},
+                        {
+                            name:"avaliable", 
+                            type:"boolean", 
+                            default: true
+                        },
+                        {
+                            name:"licensePlate", 
+                            type:"varchar", 
+                            isUnique: true
+                        },
+                        {
+                            name:"fineAmount", 
+                            type:"numeric"
+        
+                        },
+                        {
+                            name:"brand", 
+                            type:"varchar"
+        
+                        },
+                        {
+                            name:"categoryId", 
+                            type:"uuid",
+                            isNullable: true
+        
+                        },
+                        {
+                            name:"createdAt", 
+                            type:"timestamp",
+                            default: "now()"
+        
+                        },
+                        
+                    ],
+                    foreignKeys: [
+                        {
+                            name:"fkCarCategoryId",
+                            referencedTableName: "categories",
+                            referencedColumnNames:["id"],
+                            columnNames:["categoryId"],
+                            onDelete: "SET NULL",
+                            onUpdate: "SET NULL"
+                        }
+                    ]
+                }
+            )
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("cars");
     }
 
 }
