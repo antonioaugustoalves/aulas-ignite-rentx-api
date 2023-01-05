b041ceb35846d2866462a2b3256f291fdd1cd529
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-01-04 21:48:39 -0300

    Aula 12: Refatorando as migrations e criando a migration de cadastro de carros

---
+++ src/shared/infra/typeorm/migrations/1672879657781-CreateCarTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCarTable1672879657781 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
