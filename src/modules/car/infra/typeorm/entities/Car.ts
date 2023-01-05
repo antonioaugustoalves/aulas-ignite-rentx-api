00c2276085c0ed8a7f4253d5cb0a6f0563a47bdd
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-01-04 22:31:49 -0300

    Aula 12: Começando o use Case de cadastro de carros

---
+++ src/modules/car/infra/typeorm/entities/Car.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import {v4 as uuidv4} from "uuid";
+@Entity("cars")
+class Car {
+    @PrimaryColumn()
+    id: string;
+    @Column()
+    name: string;
+    @Column()
+    description: string;
+    @Column()
+    dailyRate: number;
+    @Column()
+    avaliable: boolean;
+    @Column()
+    licensePlate: string;
+    @Column()
+    fineAmount: number;
+    @Column()
+    brand: string;
+    @Column()
+    categoryId: string;
+    @CreateDateColumn()
+    createdAt: Date;
+
+    constructor(){
+        if(!this.id) {
+            this.id = uuidv4();
+        }
+    }
+
+}
